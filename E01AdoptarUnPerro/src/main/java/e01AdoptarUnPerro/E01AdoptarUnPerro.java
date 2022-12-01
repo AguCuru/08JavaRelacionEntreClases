2c1d84e833a5cea169e350ad6b30c20d2acedc44
Agustin Curutchet <agucuru@gmail.com>
2022-12-01 19:14:18 -0300

    Ejercicio02-Ejercicio03

---
+++ E01AdoptarUnPerro/src/main/java/e01AdoptarUnPerro/E01AdoptarUnPerro.java
@@ -0,0 +1,22 @@
+package e01AdoptarUnPerro;
+
+
+import Serivio.ServicioPersonaPerro;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+/**
+ *
+ * @author TefyAguFeli
+ */
+public class E01AdoptarUnPerro {
+
+    public static void main(String[] args) {
+        ServicioPersonaPerro spp = new ServicioPersonaPerro ();
+        
+        
+    }
+}
